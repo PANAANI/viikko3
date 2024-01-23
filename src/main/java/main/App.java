b15c02c0646a06f3c1caaddba635164405e64806
Joona Niemenmaa <joonaoskari@hotmail.com>
2024-01-23 08:35:32 +0200

    Commit poggers

---
+++ src/main/java/main/App.java
@@ -0,0 +1,11 @@
+package main;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int $variable;
+        $variable = 20;
+        System.out.println($variable);
+    }
+}
